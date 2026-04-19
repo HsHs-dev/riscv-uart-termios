626e455df3c07ef624ec8e54ed497c57aa089d0d
Hassan Mahmoud <officialhassansmail@gmail.com>
2026-04-19 19:35:46 +0300

    - Write documentation for uart_conf.c

--- uart_conf.c
+++ uart_conf.c
@@ -1,3 +1,21 @@
+/*
+ * A program that initializes and configure a UART (Universal asynchronous receiver-transmitter)
+ * interface on Linux using the termios library, which describe a general terminal interface that
+ * is provided to control asynchronous communications ports.
+ *
+ * The program flow is:
+ * - Open a serial device (specified by the user by giving a command-line argument,
+ *   or a virtual terminal for the auto-testing script)
+ * - Configures the UART to 8N1 data format, 115200 baud rate, and raw transfer mode
+ * - Transmits a test message over the configured serial interface
+ * - Receive and print any incoming data within a 5-seconds timeout window
+ * - Gracefully handle any errors that occur in any of the previous steps
+ * - Close the serial device and exit
+ *
+ * Author: Hassan Siddig
+ * Date: April 2026
+ */
+
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -9,9 +27,11 @@
 
 int main(int argc, char* argv[]) {
 
+
+  /* open the serial device for reading/writing in non-blocking mode */
+
   const char *term = (argc > 1) ? argv[1] : "/dev/ttyS0";
 
-  // opening the serial device for reading/writing in non-blocking mode
   int fd = open(term, O_RDWR | O_NONBLOCK);
   if (fd == -1) {
     switch (errno) {
@@ -31,7 +51,7 @@ int main(int argc, char* argv[]) {
     return EXIT_FAILURE;
   }
 
-  // check if the fd refers to a terminal
+  /* verify that the file descriptor refers to a terminal device */
   if (!(isatty(fd))) {
     switch (errno) {
       case EBADF:
@@ -48,7 +68,11 @@ int main(int argc, char* argv[]) {
     return EXIT_FAILURE;
   }
 
-  // configure the communication
+
+  /***************************************
+   * configure the UART serial interface *
+   ***************************************/
+
   struct termios conf;
 
   if (tcgetattr(fd, &conf) == -1) {
@@ -56,23 +80,27 @@ int main(int argc, char* argv[]) {
     return EXIT_FAILURE;
   }
 
-  // set the input baud rate, B115200 for compatibility
+  /* set the input/output baud rates to B115200 */
+
   if (cfsetispeed(&conf, B115200) == -1) {
     perror("cfsetispeed");
     return EXIT_FAILURE;
   }
 
-  // set the output baud rate, B115200 for compatibility
   if (cfsetospeed(&conf, B115200) == -1) {
     perror("cfsetospeed");
     return EXIT_FAILURE;
   }
 
-  // configuring control modes
+  /* 
+   * configuring control modes:
+   * - 8 data bits
+   * - no parity bit
+   * - 1 stop bit
+   */
 
-  // configure to 8N1 config
 
-  // clear the configuration
+  // clear the character size bits
   conf.c_cflag &= ~CSIZE;
 
   // set the data bits to 8
@@ -88,8 +116,13 @@ int main(int argc, char* argv[]) {
   conf.c_cflag &= ~CSTOPB;
 
 
-  // configuring local modes
-
+  /* 
+   * configuring local modes:
+   * - use noncanonical mode for direct sending and no-editing,
+   *   allowing for raw transmitting
+   * - disable echoing back data
+   * - disable signals to avoid measinterpretation
+   */
 
   // use noncanonical mode
   conf.c_lflag &= ~ICANON;
@@ -100,21 +133,29 @@ int main(int argc, char* argv[]) {
   // disable signals INTR, QUIT, [D]SUSP 
   conf.c_lflag &= ~ISIG;
 
-
-  // configuring input modes
-
+  /* 
+   * configuring input modes:
+   *
+   * - disable software flow control to avoid measinterpretation
+   *
+   */
 
   // disable flow control flags
   conf.c_iflag &= ~(IXON | IXOFF | IXANY);
 
 
-  // configuring output modes
+  /* 
+   * configuring output modes:
+   *
+   * - disable output post-processing to avoid measinterpretation
+   *
+   */
 
   // disable implementation-defined output processing (sending raw data)
   conf.c_oflag &= ~OPOST;
 
 
-  // set the configured attributes
+  /* set the configured terminal attributes immediately */
   if (tcsetattr(fd, TCSANOW, &conf) == -1) {
     switch (errno) {
       case EBADF:
@@ -134,22 +175,24 @@ int main(int argc, char* argv[]) {
     return EXIT_FAILURE;
   }
 
-  // writing data
-  char* hello = "hello from the uart_conf\n";
+  /* transmit a test message over the serial port */
+  const char* hello = "hello from the uart_conf program\n";
   ssize_t bytes_written = write(fd, hello, strlen(hello));
   if (bytes_written == -1) {
     perror("write");
     return EXIT_FAILURE;
   }
+  fprintf(stdout, "wrote %zd bytes to serial port %s\n", bytes_written, term);
 
-  // reading data
 
+  /* read a message from the serial port within a 5-seconds timeout window */
   struct pollfd pfd;
   pfd.fd = fd;
   pfd.events = POLLIN;
 
   const int timeout = 5000;
   int pollret = poll(&pfd, 1, timeout);
+  fprintf(stdout, "%d seconds to read data\n", (timeout/1000));
   if (pollret < 0) {
     perror("poll");
     return EXIT_FAILURE;
@@ -169,6 +212,8 @@ int main(int argc, char* argv[]) {
     fprintf(stdout, "Received %zd bytes: %s", read_bytes, buf);
   } 
 
+
+  /* close the serial port and exit the program */
 
   close(fd);
 
